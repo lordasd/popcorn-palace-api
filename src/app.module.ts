16b099870885b615b72cebf6eea5c15ae781a5d0
lordasd <davidzdbr@gmail.com>
2025-03-18 14:41:03 +0200

    Fixed app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { MoviesModule } from './modules/movies/movies.module';
 import { ShowtimesModule } from './modules/showtimes/showtimes.module';
 import { BookingsModule } from './modules/bookings/bookings.module';
 import { databaseProviders } from './database/database.providers/database.providers';
-import { movieProviders } from './modules/movies/movies.module/movies.providers';
+import { movieProviders } from './modules/movies/movie.providers/movie.providers';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [MoviesModule, ShowtimesModule, BookingsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService, databaseProviders, movieProviders],
+  providers: [AppService, ...databaseProviders, ...movieProviders],
 })
 export class AppModule {}
