81e259afdf1e126714e4f59ca34f015e75d61f27
lordasd <davidzdbr@gmail.com>
2025-03-18 12:19:35 +0200

    Adding movies, showtime, and bookings basic files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './modules/movies/movies.module';
+import { ShowtimesModule } from './modules/showtimes/showtimes.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule, ShowtimesModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
