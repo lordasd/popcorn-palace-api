c2ace1c4b298786fd868984f1c673a68fa38b9a0
lordasd <Davidzdbr@gmail.com>
2025-03-21 18:19:47 +0200

    Added some class validators/transormer to the dto of create-booking

--- src/modules/bookings/dto/create-booking.dto.ts
+++ src/modules/bookings/dto/create-booking.dto.ts
@@ -1,12 +1,23 @@
-import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateBookingDto {
-  @IsString()
+  @IsNumber()
   @IsNotEmpty()
+  @Type(() => Number)
+  @IsPositive()
   showtimeId: number;
 
-  @IsInt()
-  @Min(1)
+  @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number)
+  @Min(1, { message: 'Seat number must be a positive integer' })
   seatNumber: number;
 
   @IsString()
